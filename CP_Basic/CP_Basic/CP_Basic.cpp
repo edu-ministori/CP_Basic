6221f07a782a0f5eb5644fe2d1aca7f6622a1409
edu-ministori <edu.ministori@gmail.com>
2023-11-20 17:54:01 +0900

    CP Arithmetic

--- CP_Basic/CP_Basic/CP_Basic.cpp
+++ CP_Basic/CP_Basic/CP_Basic.cpp
@@ -95,6 +95,50 @@ int main()
 	Inc *= 3;
 	Inc /= 3;
 	Inc %= 3;
+
+	/*
+		비교 연산
+		
+		크기비교의 결과는 참/거짓 => 실제 출력 1/0
+
+		true : 참 : 1
+		false : 거짓 : 0
+	*/
+
+
+	int Compare1 = 10;
+	int Compare2 = 20;
+
+	// > : 크다, < : 작다 => 방향이 고정
+	cout << (Compare1 > Compare2) << endl;
+	cout << (Compare1 < Compare2) << endl;
+	cout << (Compare1 >= 100) << endl;
+	cout << (Compare1 == Compare2) << endl;
+	cout << (Compare1 != Compare2) << endl;
+
+	// 논리 연산 : bool 데이터 연산
+	/*
+		AND : && ( & : ampersand )
+
+		true && true : true
+		true && false : false
+		=> bool 데이터가 모두 true일 때 결과가 true
+
+		OR : ||  ( | : pipe )
+
+		false || true : true
+		false || false : false
+		=> bool 데이터 중 하나만 true이면 결과가 true
+
+		NOT : !
+
+		!(false) => true
+
+		bool 데이터의 결과값을 반대로 부정
+	*/
+
+	cout << ( (Compare1 < Compare2) && (Compare1 < 50) ) << endl;
+	cout << (!(Compare1 < Compare2)) << endl;
 }
 
 // 비트와 바이트에 대한 이해
