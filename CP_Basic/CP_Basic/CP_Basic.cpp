2c06aad8c26fb1d79d8d8a27bc5a2778df8b3b08
edu-ministori <edu.ministori@gmail.com>
2023-11-21 11:31:37 +0900

    제어문

--- CP_Basic/CP_Basic/CP_Basic.cpp
+++ CP_Basic/CP_Basic/CP_Basic.cpp
@@ -139,6 +139,163 @@ int main()
 
 	cout << ( (Compare1 < Compare2) && (Compare1 < 50) ) << endl;
 	cout << (!(Compare1 < Compare2)) << endl;
+
+	int Condition1 = 100;
+
+	/*
+		if가 포함하는 조건식의 원리
+		: 결과값이 bool 데이터
+
+		조건식의 종류
+		- 비교연산식
+		- bool 데이터
+		- true -> 1, false -> 0
+		- 산술 연산식
+	*/
+	if (Condition1 >= 50) {
+		//참일때 실행구문
+		cout << Condition1 << endl;
+	}
+
+	if(true){}
+	if(false){}
+	if(1){}
+	if(0){}
+	if(20){} // 0 : false, 이외의 정수 : true
+	int Condition2 = 10;
+	if(Condition2 + 5){}
+	if( Condition >= 50 && Condition2 <= 10 ){}
+
+	/*
+		if(조건식1){
+			// 조건식1이 참일때 실행 구문
+		}
+
+		if(조건식2){
+			// 조건식2가 참일때 실행구문
+		} else {
+			// 조건식2가 거짓일때 실행구문
+		}
+
+		if(조건식3){
+			// 조건식3이 참일때 실행구문
+		} else if(조건식4){
+			// 조건식4가 참일때 실행구문
+		} else if(조건식5){
+			// 조건식5가 참일때 실행구문
+		} else {
+			// 조건식이 모두 거짓일때 실행구문
+		}
+	*/
+
+	// A,B 두 수를 입력받아 비교해서 결과 출력
+	// 출력 : A>B => ">" , A<B => "<" , A=B => "=="
+	int NumA = 0;
+	int NumB = 0;
+
+	//cin >> NumA;
+	//cin >> NumB;
+
+	if (NumA > NumB) {
+		cout << ">" << endl;
+	}
+	else if (NumA < NumB) {
+		cout << "<" << endl;
+	}
+	else {
+		cout << "==" << endl;
+	}
+
+	// 시험점수 입력, 90~100:A , 80~89:B , 70~79:C , 60~69:D , 60미만:F
+	int NumTest = 0;
+	//cin >> NumTest;
+	
+	if (NumTest >= 90) {
+		cout << "A" << endl;
+	}
+	else if (NumTest >= 80) {
+		cout << "B" << endl;
+	}
+	else if (NumTest >= 70) {
+		cout << "C" << endl;
+	}
+	else if (NumTest >= 60) {
+		cout << "D" << endl;
+	}
+	else {
+		cout << "F" << endl;
+	}
+
+	// 상점에서 도둑이 들었는데, 목격자가 인상착의를 말했다.
+	// 키는 약 180이상, 옷은 검정색 옷을 입고 있었다고 말했다.
+	// 범인을 찾는 조건문을 작성하시오.
+	// 옷색 입력값 : Black = B, White = W , Red = R 
+
+	int TallNumber = 0;
+	char ClothColor = NULL; // NULL : 문자값이 존재하지 않음
+
+	cin >> TallNumber;
+	cin >> ClothColor;
+
+	// TallNumber >= 180
+	// ClothColor == 'B'
+
+	if (TallNumber >= 180 && ClothColor == 'B') {
+		cout << "범인" << endl;
+	}
+	else {
+		cout << "시민" << endl;
+	}
+
+	// if 중첩
+	/*
+		if(조건식1){
+			// 참일때 실행
+			if(조건식2){
+			}
+		}
+	*/
+	/*
+	* 180이상이고 검정색 옷 : 범인
+	* 180이하이고 검정색 옷 : 용의자
+	* 180이상이고 다른색 옷 : 용의자
+	* 두 조건 모두 아니면 : 시민
+	*/
+
+	if (TallNumber >= 180 && ClothColor == 'B') {
+		cout << "범인" << endl;
+	}
+	else if (TallNumber >= 180 || ClothColor == 'B') {
+		cout << "용의자" << endl;
+	}
+	else {
+		cout << "시민" << endl;
+	}
+
+	// 중첩
+	if (TallNumber >= 180) {
+		if (ClothColor == 'B') {
+			cout << "범인" << endl;
+		}
+		else {
+			cout << "용의자" << endl;
+		}
+	}
+	
+	
+	if(ClothColor == 'B'){
+		if (TallNumber >= 180) {
+			cout << "범인" << endl;
+		}
+		else {
+			cout << "용의자" << endl;
+		}
+	}
+
+	cout << "시민" << endl;
+
+
+	
 }
 
 // 비트와 바이트에 대한 이해
